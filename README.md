68e41bfca8c58aa6575c03f6657af0ada4207e83
okao <hamzathanees@gmail.com>
2021-11-13 06:33:44 +0500

    Happy Coding

---
+++ README.md
@@ -0,0 +1,2 @@
+### My First Package ! Hope this will assist for some people out there
+``` By okao.mv ```
---
+++ core/index.jsx
@@ -0,0 +1,40 @@
+import data from './islands';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import React, { Component, Fragment } from 'react';
+
+const atoll_islands = data.map(atoll_island => ({ value: `${atoll_island.atoll}.${atoll_island.name}`, label: `${atoll_island.atoll}.${atoll_island.name}` }))
+const animatedComponents = makeAnimated();
+
+
+export default class SingleSelect extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Select
+          className="mv-islands"
+          classNamePrefix="select"
+          defaultValue={atoll_islands[0]}
+          isDisabled={false}
+          isLoading={false}
+          isClearable={true}
+          isRtl={false}
+          isSearchable={true}
+          options={atoll_islands}
+          components={animatedComponents}
+        />
+
+        <div
+          style={{
+            color: 'hsl(0, 0%, 40%)',
+            display: 'inline-block',
+            fontSize: 12,
+            fontStyle: 'italic',
+            marginTop: '1em',
+          }}
+        >
+        </div>
+      </Fragment>
+    );
+  }
+}
